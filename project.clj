6ad5eb0245f040052042412d9f6906cd1dcc581b
Max Penet <max.penet@exoscale.ch>
2021-07-22 12:11:40 +0200

    Version 1.0.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/fn "1.0.0-alpha1-SNAPSHOT"
+(defproject cc.qbits/fn "1.0.0-alpha1"
   :url "https://github.com/mpenet/fn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
